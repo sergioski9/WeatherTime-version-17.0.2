8166a18baeb24d559766aa02295212e08cc656ea
sergio rodas <rodascruzsergio@gmail.com>
2022-06-07 15:30:50 -0400

    added components Weather, most files cityinfo, stories, test and index

---
+++ src/components/Weather/Weather.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
+import IconState, { validValues } from './../IconState'
+import { IconContext } from 'react-icons'
+
+const Weather = ({ temperature, state }) => {
+  return (
+    <div>
+      <IconContext.Provider value={{ size: '4em' }}>
+        <IconState state={state} />
+      </IconContext.Provider>
+      <Typography display='inline' variant='h2'>{temperature}</Typography>
+    </div>
+  )
+}
+
+Weather.propTypes = {
+  temperature: PropTypes.number.isRequired,
+  state: PropTypes.oneOf(validValues).isRequired
+}
+
+export default Weather

4f5009aa7d49221622ffb41838d41e1b70d2875c
sergio rodas <rodascruzsergio@gmail.com>
2022-06-07 15:28:15 -0400

    added app.jsx and most config in the app test and index

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router,
+  Switch,
+  Route } from 'react-router-dom'
+import WelcomePage from './pages/WelcomePage'
+import MainPage from './pages/MainPage'
+import CityPage from './pages/CityPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <WelcomePage />
+          </Route>
+          <Route path='/main'>
+            <MainPage />
+          </Route>
+          <Route path='/city'>
+            <CityPage />
+          </Route>
+          <Route>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
